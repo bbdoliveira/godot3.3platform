1c658a438d15376652e66dd67229089e5746d6ce
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-05-18 00:49:20 -0300

    Adicionando animações

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,11 +8,19 @@ var jump_force = -720
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	
-	var move_direction = int (Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	_get_input()
+	
 
 	if Input.is_action_pressed("jump"):
 		velocity.y = jump_force / 2
 	
-	velocity.x = move_speed * move_direction
 
 	move_and_slide(velocity)
+	
+func _get_input():
+	var move_direction = int (Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))	
+	velocity.x = move_speed * move_direction
+	
+	#Verifica para qual lado o player vai e vira a textura.
+	if move_direction !=0:
+		$texture.scale.x = move_direction
