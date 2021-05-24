907a3306e566882eee3fc7e565878850e33ac007
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-05-24 14:54:23 -0300

    Finalizando movimentação básica inimigo

--- Scripts/Mushroom.gd
+++ Scripts/Mushroom.gd
@@ -1,3 +1,27 @@
 extends KinematicBody2D
 
+export var speed = 64
+export var health = 1
+var velocity = Vector2.ZERO
+var move_direction = -1
 
+func _physics_process(delta):
+	velocity.x = speed * move_direction
+	
+	velocity = move_and_slide(velocity)
+
+	if move_direction == 1: #Inverte o sprite para o lado que estetiver andando.
+		$texture.flip_h = true
+	else:
+		$texture.flip_h = false
+#
+	if $ray_wall.is_colliding():
+		$anim.play("idle")
+
+
+func _on_anim_animation_finished(anim_name):
+	if anim_name == "idle":
+		$texture.flip_h != $texture.flip_h
+		$ray_wall.scale.x *= -1
+		move_direction *= -1
+		$anim.play("run")
